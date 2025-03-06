f211bf25b2db4a92e92a783f4f2ab0e45e48bd14
anshallen <ansh.parmar@allen.in>
2025-03-07 02:39:22 +0530

    feat: add group creation API with handlers and services

--- models/group.go
+++ models/group.go
@@ -19,6 +19,6 @@ type Group struct {
 type Message struct {
 	ID        string    `json:"id"`
 	Content   string    `json:"content"`
-	Sender    User      `json:"sender"`
+	SenderId  string    `json:"senderId"`
 	Timestamp time.Time `json:"timestamp"`
 }
