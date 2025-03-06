b97d9894522fb4a5cb06b4ed37c4d66a6cdff6f5
Mayuri <mayuri@LT-BEN-29100.local>
2025-03-07 05:05:31 +0530

    matching changes

--- models/group.go
+++ models/group.go
@@ -3,17 +3,18 @@ package models
 import "time"
 
 type Group struct {
-	ID            string    `json:"id"`
-	Title         string    `json:"title"`
-	Description   string    `json:"description"`
-	Members       []string  `json:"members"`
-	Tag           string    `json:"tag"`
-	Type          string    `json:"type"`
-	Private       bool      `json:"private"`
-	Messages      []Message `json:"messages"`
-	CreateBy      string    `json:"createBy"`
-	Capacity      int       `json:"capacity"`
-	ActivityScore int       `json:"activityScore"`
+	ID             string    `json:"id"`
+	Title          string    `json:"title"`
+	Description    string    `json:"description"`
+	Members        []string  `json:"members"`
+	Tag            string    `json:"tag"`
+	Type           string    `json:"type"`
+	Private        bool      `json:"private"`
+	Messages       []Message `json:"messages"`
+	CreateBy       string    `json:"createBy"`
+	Capacity       int       `json:"capacity"`
+	ActivityScore  int       `json:"activityScore"`
+	MeetingStarted bool      `json:"meetingStarted"`
 }
 
 type Message struct {
